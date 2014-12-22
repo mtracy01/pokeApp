a09e4d7b31ae4af1a70207d8a3913cc830ed12b1
mtracy01 <matthetracy@gmail.com>
2014-12-22 17:14:30 -0500

    Poke app 1.0 Completed

---
+++ app/src/main/java/com/example/matthew/pokeapp/MainActivity2.java
@@ -0,0 +1,107 @@
+package com.example.matthew.pokeapp;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.view.animation.Animation;
+import android.widget.Button;
+import android.widget.ImageButton;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import android.widget.AdapterView;
+import com.google.android.gms.*;
+
+
+
+public class MainActivity2 extends Activity {
+
+    public static ImageButton pokeButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity2);
+        pokeButton=(ImageButton)findViewById(R.id.pokeButton);
+        pokeButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(final View view){
+
+                view.animate().translationYBy(-500);
+
+
+                try{
+                    view.wait(50);
+                }
+                catch(Exception e){}
+                Runnable endAction = new Runnable(){
+                    public void run(){
+                        view.animate().translationYBy(500);
+                    }
+                };
+                //view.animate().setDuration(1000);
+                view.animate().withEndAction(endAction);
+
+            }
+
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_activity2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        public static class AdFragment extends Fragment {
+            @Override
+            public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                     Bundle savedInstanceState) {
+                return inflater.inflate(R.layout.fragment_ad, container, false);
+            }
+
+            @Override
+            public void onActivityCreated(Bundle bundle) {
+                super.onActivityCreated(bundle);
+                AdView mAdView = (AdView) getView().findViewById(R.id.adView);
+                AdRequest adRequest = new AdRequest.Builder().build();
+                mAdView.loadAd(adRequest);
+            }
+        }
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main_activity2, container, false);
+            return rootView;
+        }
+    }
+}
